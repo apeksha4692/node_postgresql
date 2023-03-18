00e4694779fe468f15088f5549a933ab4f7c7f5f
Apeksha Mazumdar <apkesha.infograins@gmail.com>
2023-03-18 16:11:47 +0530

    changes

--- sequelize-connection.js
+++ sequelize-connection.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 
 const isProduction = process.env.NODE_ENV === "production";
 
+console.log("process.env.PGHOST",process.env.PGHOST);
 const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER,process.env.PGPASSWORD, {
     host: process.env.PGHOST,
     port: process.env.PGPORT,
